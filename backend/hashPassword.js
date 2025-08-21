aec5e6ef3187613a1b76ddf9b4fcb74b564fa5c8
rossato08 <gabriel.rossato050208@gmail.com>
2025-08-21 13:40:33 -0300

    hash passwor funcionando

--- backend/hashPassword.js
+++ backend/hashPassword.js
@@ -1,28 +1,38 @@
-import bcrypt from 'bcryptjs';
-import dotenv from 'dotenv';
+// geraHash.js
+import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 async function generateHash(label, password) {
-  // Gerar o salt
-  const salt = await bcrypt.genSalt(10);
+  if (!password) {
+    console.error(`⚠️ A senha para ${label} não foi definida.`);
+    return;
+  }
 
-  // Hashear a senha com o salt
-  const hashedPassword = await bcrypt.hash(password, salt);
+  try {
+    // Gerar o salt
+    const salt = await bcrypt.genSalt(10);
 
-  console.log(`${label} Hash:`, hashedPassword);
+    // Hashear a senha com o salt
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    console.log(`${label} Hash:`, hashedPassword);
+  } catch (error) {
+    console.error(`❌ Erro ao gerar hash para ${label}:`, error);
+  }
 }
 
 async function generateHashedPasswords() {
   try {
-    await generateHash('ADMIN', process.env.SENHA_ADMIN);
-    await generateHash('USER', process.env.SENHA_USER);
-    await generateHash('teste', 'senha123')
+    await generateHash("ADMIN", process.env.SENHA_ADMIN);
+    await generateHash("USER", process.env.SENHA_USER);
+    await generateHash("TESTE", "senha123");
 
-    process.exit(0); // Encerra o processo após exibir o hash
+    process.exit(0); // finaliza
   } catch (error) {
-    console.error('Erro ao hashear a senha:', error);
-    process.exit(1); // Encerra o processo com código de erro
+    console.error("❌ Erro inesperado:", error);
+    process.exit(1);
   }
 }
 
